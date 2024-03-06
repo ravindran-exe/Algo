207f593730bf5d44e250f1823d279df9c1a44578
ravindran-exe <ravindran2213003@ssn.edu.in>
2024-03-06 23:22:45 +0530

    Feat: Added Seive.java file

---
+++ Seive.java
@@ -0,0 +1,27 @@
+package math;
+import java.util.*;
+public class Seive {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        boolean[] primes = new boolean[n + 1];
+        sieve(n, primes);
+    }
+
+    // false in array means number is prime
+    static void sieve(int n, boolean[] primes) {
+        for (int i = 2; i * i <= n; i++) {
+            if (!primes[i]) {
+                for (int j = i * 2; j <= n; j += i) {
+                    primes[j] = true;
+                }
+            }
+        }
+
+        for (int i = 2; i <= n; i++) {
+            if (!primes[i]) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
